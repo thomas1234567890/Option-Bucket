a6f3caffa774fd2ce327b3e1f197883b82abe903
thomas1234567890 <tompatcon@gmail.com>
2022-04-01 00:15:42 +0100

    3 files modified

--- index.ts
+++ index.ts
@@ -1,10 +1,22 @@
 // Import stylesheets
 import './style.scss';
 
-const slider = document.querySelector('.input_range');
+const border_slider = document.querySelector('.input_range_border_radius');
+const height_slider = document.querySelector('.input_range_height');
+const width_slider = document.querySelector('.input_range_width');
 const root = document.documentElement;
 
-slider.addEventListener('input', (e) => {
+border_slider.addEventListener('input', (e) => {
   //@ts-ignore
   root.style.setProperty('--radius', e.target.value + '%');
+});
+
+width_slider.addEventListener('input', (e) => {
+  //@ts-ignore
+  root.style.setProperty('--width', e.target.value + 'px');
+});
+
+height_slider.addEventListener('input', (e) => {
+  //@ts-ignore
+  root.style.setProperty('--height', e.target.value + 'px');
 });
